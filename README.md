6896cb40af08caa446d7f57261bebb42e3be5917
Scott Green <47545109+nanonanouk@users.noreply.github.com>
2021-01-13 17:12:42 +0000

    Updated to support PayPal export format as of 2020-01-13
    Added --fees argument to only output the PayPal fees. This is in response to this issue with PayPal UK and QBO (https://quickbooks.intuit.com/learn-support/en-uk/banking/paypal-fees-not-showing-up-in-banking-feed/00/518988/page/3).

--- README.md
+++ README.md
@@ -6,13 +6,14 @@ Converts CSV exports from Paypal to a CSV format that QuickBooks Online understa
 Paypal Download
 ---------------
 
-1. In your Paypal account, browse to the History → Download History page.
-2. Select the "Comma Delimeted - Balance Affecting Payments" format and either download a time range or use the Last Download to Present option. Either way, you'll end up with a CSV file (probably called `Download.csv`).
+1. In your Paypal account, browse to the Activity → All transactions -> Download.
+2. Select the CSV Format and either download a time range or use the Last Download to Present option. Either way, you'll end up with a CSV file (probably called `Download.CSV`).
 
 Convert
 -------
 
-1. Run `python paypal2qbo.py Download.csv` (note that if you have multiple CSV files from Paypal you can add specify them all). The script will output a file named `QuickBooksOutput.csv`.
+1. Run `python paypal2qbo.py Download.CSV` (note that if you have multiple CSV files from Paypal you can add specify them all). The script will output a file named `QuickBooksOutput.csv`.
+2. If you only want to output the fees do `python paypal2qbo.py --fees Download.CSV`.
 
 QuickBooks Online Upload
 ------------------------
--- paypal2qbo.py
+++ paypal2qbo.py
@@ -4,27 +4,32 @@ import time
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('infile', nargs='+', type=argparse.FileType('r'))
+    parser.add_argument('infile', nargs='+', type=argparse.FileType('r', encoding='utf-8-sig'))
+    parser.add_argument('--fees',
+                        action='store_true',
+                        help="only add fees")
     args = parser.parse_args()
 
     transactions = []
     for f in args.infile:
         for line in csv.DictReader(f):
+
             # The single line has a gross payment and a fee payment, so we need to split those apart
-            tstamp = time.strptime('{Date} { Time}'.format(**line), '%m/%d/%Y %H:%M:%S')
+            tstamp = time.strptime('{Date} {Time}'.format(**line), '%m/%d/%Y %H:%M:%S')
 
             # This is the "gross" payment
-            transactions.append((
-                tstamp,
-                '{ Type} From { Name}'.format(**line),
-                line[' Gross'].replace(',', '')
-            ))
-
-            if line[' Fee'] != '0.00':
+            if not args.fees:
                 transactions.append((
                     tstamp,
-                    '{ Type} Fee From { Name}'.format(**line),
-                    line[' Fee'].replace(',', '')
+                    '{Type} for {Item Title} from {Name}'.format(**line),
+                    line['Gross'].replace(',', '')
+                ))
+
+            if line['Fee'] != '0.00':
+                transactions.append((
+                    tstamp,
+                    '{Type} for {Item Title} from {Name} (PayPal fees)'.format(**line),
+                    line['Fee'].replace(',', '')
                 ))
 
     with open('QuickBooksOutput.csv', 'w') as f:
